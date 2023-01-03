e9fe356a2e96371380b03f4dd609bfaf5826f4ea
ThiagoAmaru <thiago410psi@gmail.com>
2023-01-03 10:06:17 -0300

    adicionando mais exercicios

--- exemplo01.py
+++ exemplo01.py
@@ -3,7 +3,6 @@ import pandas as pd
 data = pd.read_csv('dataset/kc_house_data.csv')
 
 #mostra as colunas desejadas, de maneira ordenada, a partir da coluna escolhida, neste caso do valor maior para o menor
-tamanho = (data.shape)
-print(tamanho)
-print(tamanho[0])
-print(tamanho[1])
+preco = data[['id', 'price']].sort_values('price', ascending = False)
+print(preco.iloc[0])
+print(preco)
--- projeto01.py
+++ projeto01.py
@@ -4,15 +4,25 @@ import pandas as pd
 data = pd.read_csv('dataset/kc_house_data.csv')
 
 #Obtendo o número de linhas e colunas
-tamanho = (data.shape)
-print(tamanho)
-print(tamanho[0])
-print(tamanho[1])
+tamanho = data.shape
+atributos = data.columns
+preco = data[['id', 'price']].sort_values('price', ascending = False)
+quartos = data[['id', 'bedrooms']].sort_values('bedrooms', ascending = False)
 
 
-print('P: Quantas casas estão disponiveis para compra?')
+print('01P: Quantas casas estão disponiveis para compra?')
 print('R= Estão disponiveis', tamanho[0], 'casas para a compra')
 print('='*70)
-print('P: Quantos atributos as casas possuem?')
+print('02P: Quantos atributos as casas possuem?')
 print('R= As casas possueme', tamanho[0], 'atributos')
 print('='*70)
+print('03P: Quais os atributos das casas?')
+print('R= Os atributos das casas analisados são:')
+for x in atributos: print(x, end=", ")
+print('\n','='*70)
+print('04P: Qual a casa mais cara?')
+#verificar outra alternativa para essa questão, existem falhas
+print('R= A casa mais cara é a', preco.iloc[0])
+print('='*70)
+print('05P: Qual a casa maior numero de quartos?')
+print('R= A casa mais cara é a', quartos.iloc[0])
